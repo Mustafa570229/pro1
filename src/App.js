6ec7547e35d860ba7fed07a7ff1a4a31fa20b9be
mustafa alabohasne <118037442+Mustafa570229@users.noreply.github.com>
2022-11-21 02:17:47 +0300

    effect and state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React from "react";
-import NavBar from "./components/NavBar";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
-import SideBar from "./components/SideBar";
-import NotFound from "./components/NotFound";
-import Home from "./components/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react'
+//import State from './components/State'
+import UseEffectEx from './components/useEffectEx'
+
+
 const App = () => {
   return (
     <div>
-      <NavBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/content" element={<Content />}></Route>
-          <Route path="/footer" element={<Footer />}></Route>
-          <Route path="/sidebar" element={<SideBar />}></Route>
-          <Route path="*" element={<NotFound/>}></Route>
-          
-        </Routes>
-      </BrowserRouter>
+   <UseEffectEx/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
